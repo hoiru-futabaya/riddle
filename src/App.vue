f31c6a5e3e4d4d19afa4b97834eccef620d41352
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-10-07 13:53:54 +0900

    基本構造作った

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <Main/>
+  <Form/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
+import Form from './components/Form.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Main,
+    Form
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style>
+div {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Form.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="AnswerForm">
+    <div>
+      <div>
+        <input class="input" id="answer">
+        <input @click="getAnswer" type="button" value="enter">
+        <p v-if="answer === result">Success!</p>
+        <p v-else-if="answer === ' '"></p>
+        <p v-else>Failed...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnswerForm',
+  data () {
+    return {
+      title: 'Form',
+      answer: ' ',
+      result: ''
+    }
+  },
+  methods: {
+    getAnswer () {
+      this.axios.get('https://script.google.com/macros/s/AKfycbwJixS3l4KZDubUiU5jHDFNL11YaYxB9fngPUDI5b6MHeQJQA8/exec')
+        .then((response) => {
+          this.answer = document.getElementById('answer').value
+          var result = JSON.stringify(response.data[0].answer)
+          this.result = result.replace(/"/g, '')
+        })
+        .catch((e) => {
+          alert(e)
+        })
+    }
+  }
+}
+</script>
