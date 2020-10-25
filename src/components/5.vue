5a6c3873aea01f5838156d55c904bc51f53347b9
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-10-25 13:00:10 +0900

    5問目できた

--- src/components/5.vue
+++ src/components/5.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="main">
     <h2 id="title">
-    5
+    Ｖ<b v-on:click="l = 1">{{ monme[l] }}</b>
     </h2>
-    ここに問題を表示
+    <p id="text">Tap or click, and
+    you can "<b v-on:click="k = 1">{{ lightup[k] }}</b>".</p>
+    <br>
+    <h4>泣いた拍子に<b v-on:click="n = 1">{{ same[n] }}</b>たが悔しい 夢と知ったら泣かぬのに</h4>
+    <h4>門松は <b v-on:click="m = 1">{{ meido[m] }}</b>の旅の 一里塚 めでたくもあり めでたくもなし</h4>
+    <h4><b v-on:click="o = 1">{{ kuro[o] }}</b>する身は何いとわねど 苦労しがいのあるように</h4>
+    <h5>ヒント・2019/12/06 初配信</h5>
+
   </div>
 </template>
 
 <script>
 export default {
-  title: 'Page5'
+  title: 'Page5',
+  data () {
+    return {
+      k: 0,
+      l: 0,
+      n: 0,
+      m: 0,
+      o: 0,
+      lightup: ['light up', 'らいとあっぷ'],
+      monme: ['問目', 'tuber'],
+      same: ['覚め', '鮫'],
+      meido: ['冥土', 'メイド'],
+      kuro: ['苦労', 'クロワッサン']
+    }
+  }
 }
 </script>
 
 <style>
+b {
+color: #ff00ff;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,10 @@
     <h2 id="title">
     りどびゅー！
     </h2>
-    <p id="text">全5問。JavaScriptのフレームワークであるVue.jsの勉強用に作りました。</p>
+    <h3>
+    全5問
+    </h3>
+    <p id="text">JavaScriptのフレームワークであるVue.jsの勉強用に作りました。</p>
     <p id="text">riddle（なぞなぞ）+Vue.jsでりどびゅー！です（笑）</p>
     <p id="text">入力フォームに正しい答えを入力すると次の問題に進めます。</p>
     <p id="text">まずは「start」と入力してスタート！</p>
