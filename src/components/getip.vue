789e2ee64b4bfe2b47881d5a566d4673e644687a
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-11-07 17:22:50 +0900

    ビルド

--- src/components/getip.vue
+++ src/components/getip.vue
@@ -23,7 +23,7 @@ export default {
       }
       )
       .then(ip => {
-        this.ipAddress = ip.ip
+        this.ipAddress = ip
         axios
           .get('https://script.google.com/macros/s/AKfycbwMZwR-7TSCJ79fgfc8yiU37at5fpt6wcVDMTI7Weik-bYqxBaH/exec?name=riddle&ip=' + ip.ip)
           .then(response => (this.count = response.data.count))
